10d4f0fcc720a26413476de2ce92fa4a22a0bcef
MobeenGit <1221104836@student.mmu.edu.my>
2024-09-10 17:01:00 +0800

    added quiz for chapter 3 biology

---
+++ bioquiz3.js
@@ -0,0 +1,35 @@
+function submitQuiz() {
+    var score = 0;
+    var totalQuestions = 5;
+
+    // Question 1
+    var q1 = document.querySelector('input[name="q1"]:checked');
+    if (q1 && q1.value === 'ileum') {
+        score++;
+    }
+
+    // Question 2
+    var q2 = document.querySelector('input[name="q2"]:checked');
+    if (q2 && q2.value === 'diffusion') {
+        score++;
+    }
+
+    var q3 = document.querySelector('input[name="q3"]:checked');
+    if (q3 && q3.value === 'a cell used for absorption') {
+        score++;
+    }
+
+    var q4 = document.querySelector('input[name="q4"]:checked');
+    if (q4 && q4.value === 'A liver cell has a membrane. ') {
+        score++;
+    }
+
+    var q5 = document.querySelector('input[name="q5"]:checked');
+    if (q5 && q5.value === 'carnivores') {
+        score++;
+    }
+
+    // Display the result
+    var resultText = `You got ${score} out of ${totalQuestions} correct!`;
+    document.getElementById('result').innerText = resultText;
+}
