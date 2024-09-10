1567723de8147dae4e62fd706f94f6ac597702ee
MobeenGit <1221104836@student.mmu.edu.my>
2024-09-10 18:06:00 +0800

    made quiz questions for chapter 3 biology and other tiny things

--- bioquiz3.js
+++ bioquiz3.js
@@ -10,22 +10,22 @@ function submitQuiz() {
 
     // Question 2
     var q2 = document.querySelector('input[name="q2"]:checked');
-    if (q2 && q2.value === 'diffusion') {
+    if (q2 && q2.value === 'respiration') {
         score++;
     }
 
     var q3 = document.querySelector('input[name="q3"]:checked');
-    if (q3 && q3.value === 'a cell used for absorption') {
+    if (q3 && q3.value === 'diffusion of oxygen') {
         score++;
     }
 
     var q4 = document.querySelector('input[name="q4"]:checked');
-    if (q4 && q4.value === 'A liver cell has a membrane. ') {
+    if (q4 && q4.value === 'diffusion') {
         score++;
     }
 
     var q5 = document.querySelector('input[name="q5"]:checked');
-    if (q5 && q5.value === 'carnivores') {
+    if (q5 && q5.value === 'plant roots absorbing water from the soil') {
         score++;
     }
 
